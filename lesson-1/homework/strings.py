aeb37caa4f5cd23586ae55605a5cb9c375a64241
Abu148 <abdulaziz.mominxoja@gmail.com>
2025-02-25 17:25:03 +0500

    Update strings.py

--- lesson-1/homework/strings.py
+++ lesson-1/homework/strings.py
@@ -1,1 +1,31 @@
+task1:
+a = input("side a: ")
+b = input("side b: ")
+perimeter = (int(a) + int(b)) * 2
+yuzasi = int(a) * int(b) 
+print(perimeter)
+print(yuzasi) 
 
+task2:
+D = input("diameter size: ")
+P = 3.1416 
+length = (int(D)* int(P))
+print(length)
+
+task3: 
+a = input("a number: ")
+b = input("number b: ")
+mean = (int(a) + int(b)) / 2
+print(mean)
+
+task4:
+a = input("a number: ")
+b = input("b number: ")
+sum = (int(a) + int(b))
+product = (int(a) * int(b)) 
+square_a = (int(a) ** 2)
+square_b = (int(b) ** 2) 
+print(sum)
+print(product)
+print(square_a)
+print(square_b)
