3f22e9a549936db6484244f9189ab733814aeb35
Abu148 <abdulaziz.mominxoja@gmail.com>
2025-02-25 17:32:26 +0500

    Update exception_file_handling.py

--- lesson-6/homework/exception_file_handling.py
+++ lesson-6/homework/exception_file_handling.py
@@ -1,1 +1,40 @@
+# // Diamond // 
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(' ',end=' ')
+    for j in range(i+1):
+        print('*',end=' ')
+    for j in range(i):
+        print('*',end=' ') 
+    print()
+n = 5
+for i in range(n - 1, 0, -1):
+     for j in range(n, i - 1, -1):
+        print(' ', end=' ')
+     for j in range(i):
+        print('*', end=' ')
+     for j in range(i - 1):
+        print('*', end=' ')
+     print()                   
 
+
+
+#  // Butterfly //  
+n = 5
+for i in range(n):
+    for j in range(i + 1):
+        print('*', end=' ')
+    for j in range(2 * (n - i - 1)):
+        print(' ', end=' ')
+    for j in range(i + 1):
+        print('*', end=' ')
+    print()
+for i in range(n - 1, -1, -1):
+    for j in range(i + 1):
+        print('*', end=' ')
+    for j in range(2 * (n - i - 1)):
+        print(' ', end=' ')
+    for j in range(i + 1):
+        print('*', end=' ')
+    print()                  
